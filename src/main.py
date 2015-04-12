d2671932dda73b0e308f060668aee9cebb3ede5b
grsakea <grsakea@users.noreply.github.com>
2015-04-12 22:45:08 +0200

    Removed NaCl test

--- src/main.py
+++ src/main.py
@@ -4,22 +4,9 @@ import sys
 from PyQt5.QtWidgets import QApplication
 import window
 
-import libnacl.secret
-import libnacl.dual
-import libnacl.public
-
 
 def main():
-    message = b"TESTTTTT"
-
-    key = libnacl.public.SecretKey()
-
-    box = libnacl.public.Box(key.sk, key.pk)
-
-    encrypted = box.encrypt(message)
-    decrypt = box.decrypt(encrypted)
-
-    print(decrypt.decode('utf-8'))
+    startGUI()
 
 
 def startGUI():
