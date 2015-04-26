b7b65e65de38249a9cf3ac441698452976acac56
grsakea <grsakea@users.noreply.github.com>
2015-04-26 18:45:56 +0200

    Added support for displaying time

--- src/gtweet.py
+++ src/gtweet.py
@@ -1,12 +1,23 @@
 import requests
-from PyQt5.QtWidgets import QWidget, QLabel, QGridLayout, QTextBrowser, QFrame
+from PyQt5.QtWidgets import QWidget, QLabel, QGridLayout, QTextBrowser,\
+        QFrame, QSizePolicy
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtCore
+from datetime import timezone
 
 
 class StatusWidget(QWidget):
     def __init__(self, tweet):
         super().__init__()
+
+        if hasattr(tweet.status, 'retweeted_status'):
+            self.rt = True
+            self.st = tweet.status.retweeted_status
+            self.rtst = tweet.status
+        else:
+            self.rt = False
+            self.st = tweet.status
+
         self.initUI(tweet)
 
     def getPix(self, url, scaled=False):
@@ -32,36 +43,53 @@ class StatusWidget(QWidget):
                            i['display_url'] + "</a>"
                 status.text = status.text.replace(to_rep, in_place)
 
-                print("TO REP: " + to_rep + " " + in_place)
         print("Orig : " + orig_text)
+
+    def add_pic(self):
+        if self.rt:
+            pict1 = self.getPix(self.st.user.profile_image_url_https, False)
+            pict2 = self.getPix(self.rtst.user.profile_image_url_https, True)
+            self.lay.addWidget(pict1, 0, 0, 2, 1)
+            self.lay.addWidget(pict2, 1, 0)
+        else:
+            pict = self.getPix(self.st.user.profile_image_url_https, False)
+            self.lay.addWidget(pict, 0, 0, 2, 1)
+
+    def add_time(self):
+        time = self.st.created_at.replace(tzinfo=timezone.utc)\
+                .astimezone(tz=None)
+        string = time.strftime("%d/%m - %H:%M")
+
+        self.lay.addWidget(QLabel(string), 0, 5)
 
     def initUI(self, tweet):
         layout = QGridLayout()
+        self.setLayout(layout)
+        self.lay = layout
+
+        layout.setColumnStretch(10, 100)
+
+        self.add_pic()
+        self.add_time()
 
         if hasattr(tweet.status, 'retweeted_status'):
-            st = tweet.status.retweeted_status
-            rtst = tweet.status
-            pict1 = self.getPix(st.user.profile_image_url_https, False)
-            pict2 = self.getPix(rtst.user.profile_image_url_https, True)
-            layout.addWidget(pict1, 0, 0, 2, 1)
-            layout.addWidget(pict2, 1, 0)
-            name = '<b>' + st.user.name + '</b> <i>@' + st.user.screen_name +\
-                   '</i> RT by: <b>' + rtst.user.name + '</b>'
+            name = '<b>' + self.st.user.name + '</b> <i>@' +\
+                    self.st.user.screen_name +\
+                   '</i> RT by: <b>' + self.rtst.user.name + '</b>'
         else:
-            st = tweet.status
-            pict = self.getPix(st.user.profile_image_url_https, False)
-            layout.addWidget(pict, 0, 0, 2, 1)
-            name = "<b>" + st.user.name + "</b> <i>@" + st.user.screen_name +\
-                   "</i>"
+            name = "<b>" + self.st.user.name + "</b> <i>@" +\
+                    self.st.user.screen_name + "</i>"
 
-        self.process_text(st)
+        self.process_text(self.st)
 
         text = QTextBrowser()
-        text.setHtml(st.text)
+        text.setHtml(self.st.text)
         text.setOpenExternalLinks(True)
+        text.setFixedSize(500, 48)
+        text.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
 
         layout.addWidget(QLabel(name), 0, 1)
-        layout.addWidget(text, 1, 1)
+        layout.addWidget(text, 1, 1, 1, -1)
 
         line = QFrame()
         line.setFrameShape(QFrame.HLine)
