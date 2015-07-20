172426c4b944ecdebb7942204c3e4a1769e4751e
grsakea <grsakea@users.noreply.github.com>
2015-07-20 23:48:28 +0200

    Removed unused read variable in Tweet class

--- src/models.py
+++ src/models.py
@@ -3,7 +3,6 @@ class Tweet():
     def __init__(self, status):
         self.status = status
         self.tid = status.id
-        self.read = False
 
     def __lt__(self, other):
         return self.tid < other.tid
--- src/server_storage.py
+++ src/server_storage.py
@@ -1,3 +1,11 @@
 class Storage:
     def __init__(self):
         self.tweets = []
+        self.ressource = {}
+
+    def add_tweet(self, tweet):
+        self.tweets.append(tweet)
+
+    def add_ressource(self, url):
+        print("lol")
+# TODO dl ressource add to storage
