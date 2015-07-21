6f0c18976df25a647950db32c8cfa0be85a4307d
grsakea <grsakea@users.noreply.github.com>
2015-07-21 20:31:18 +0200

    Bugfix for StreamHandler

--- src/server_main.py
+++ src/server_main.py
@@ -9,12 +9,11 @@ from tweepy import Stream
 from models import Tweet
 from server_storage import Storage
 
-store = Storage()
-
 
 class StreamHandler(StreamListener):
-    def __init__(self):
+    def __init__(self, store):
         self.tweets = store.tweets
+        super().__init__()
 
     def on_status(self, status):
         self.tweets.append(Tweet(status))
@@ -22,6 +21,10 @@ class StreamHandler(StreamListener):
 
     def on_error(self, status):
         print(status)
+        return True
+
+    def on_timeout(self):
+        print("Timeouted")
 
 
 def load_auth():
@@ -40,22 +43,23 @@ def load_auth():
     return auth
 
 
-def start_stream(auth):
-    l = StreamHandler()
+def start_stream(auth, store):
+    l = StreamHandler(store)
     stream = Stream(auth, l)
     stream.userstream(async=True)
     return stream
 
 
 def main():
+    store = Storage()
     auth = load_auth()
     api = tweepy.API(auth)
-    st = api.home_timeline(count=20)
+    st = api.home_timeline(count=200)
     for i in st:
         store.tweets.append(Tweet(i))
     store.tweets.sort()
     print("Tweets Downloaded")
-    start_stream(auth)
+    start_stream(auth, store)
     print("Starting Server")
     server_bottle.launch_server(store)
 
