9b9e05d6659f505e130cbe8c07d2145341a0da8f
grsakea <grsakea@users.noreply.github.com>
2015-09-26 10:50:09 +0200

    Remove logging

--- src/models.py
+++ src/models.py
@@ -28,10 +28,8 @@ class Tweet():
                     self.ent['vid'].append((i['url'], self.choose_video(i)))
                 else:
                     self.ent['pic'].append((i['url'], i['media_url_https']))
-        print(self.status.text)
         if 'urls' in self.status.entities:
             for i in self.status.entities['urls']:
-                print(i)
                 self.ent['url'].append((i['indices'], i['display_url'],
                                        i['expanded_url']))
 
