ca0d03bd007387de8a5b969121001d6e935c6891
grsakea <grsakea@users.noreply.github.com>
2015-10-10 10:28:15 +0200

    Add logging

--- src/cache.py
+++ src/cache.py
@@ -7,8 +7,10 @@ class Cache:
 
     def get_resource(self, url):
         if url in self.r:
+            print("Cache hit", url)
             return self.r[url]
         else:
+            print("Cache miss", url)
             r = requests.post('http://127.0.0.2:8080/content',
                               {'resource': url})
             data = r.content
